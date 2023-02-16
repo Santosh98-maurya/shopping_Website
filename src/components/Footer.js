ba96cdfc641b91030e650ea1c6f8a82f14944bcf
Santosh Maurya <60089141+Santosh98-maurya@users.noreply.github.com>
2023-02-16 14:30:11 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -90,7 +90,7 @@ import {
     return (
       <Container>
         <Left>
-          <Logo>ADITYA</Logo>
+          <Logo>Santosh</Logo>
           <Desc>
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
